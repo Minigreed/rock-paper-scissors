5ccac69edb5d2f79237c9bce4760a5f128b98b38
Minigreed <206389157+Minigreed@users.noreply.github.com>
2026-02-20 19:51:34 -0600

    finished

--- script.js
+++ script.js
@@ -1,13 +1,69 @@
+function getHumanChoice(){
+    const humanChoice = prompt("Choose Rock, Paper, or Scissors.");
+    return humanChoice;
+}
+
 function getComputerChoice(){
     const randomNumber = Math.random();
 
     if (randomNumber < 1/3){
-        return "rock";
+        return "rock"
     } else if (randomNumber < 2/3){
-        return "paper";
-    } else { 
+        return "paper"
+    } else
         return "scissors";
-    }
 }
 
-console.log(getComputerChoice());
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice === computerChoice){
+        console.log("Tie");
+    }
+     else if (
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ){ 
+        humanScore++;
+        console.log("You Win");
+        return "human";
+    } 
+    else {
+        computerScore++;
+        console.log("You Lose");
+        return "computer";
+    }
+
+    
+}
+
+let roundOne = playRound(getHumanChoice(), getComputerChoice());
+let roundTwo = playRound(getHumanChoice(), getComputerChoice());
+let roundThree = playRound(getHumanChoice(), getComputerChoice());
+let roundFour = playRound(getHumanChoice(), getComputerChoice());
+let roundFive = playRound(getHumanChoice(), getComputerChoice());
+
+console.log(`You: ${humanScore}, Computer: ${computerScore}`);
+
+if (humanScore > computerScore){
+    console.log("You Won");
+} else if(computerScore > humanScore){
+    console.log("You Lost");
+} else {
+    console.log("It was a tie");
+};
+
+}
+
+playGame();
+
+
+
+
+
+
